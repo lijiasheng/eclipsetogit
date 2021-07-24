bbb2657805ba42ccbe39b37e8526dab6af29e5a5
Lijiasheng <jiashengl@hotmail.com>
2021-07-24 17:45:11 +0800

    开发分支增加了信息输出

--- springdemo/src/main/java/com/soho/tasks/TaskTest.java
+++ springdemo/src/main/java/com/soho/tasks/TaskTest.java
@@ -8,6 +8,8 @@ public class TaskTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("[Dev Branch]Begin to create spring container.....");
+		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-tasks.xml");  
         
 		System.getProperty("user.name") ;
