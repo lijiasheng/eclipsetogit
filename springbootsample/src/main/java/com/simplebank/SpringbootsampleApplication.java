1b9da58fd04389cb022fd20c846156a9dd0e1ff4
lijiasheng <jiashengl@hotmail.com>
2022-05-22 11:49:11 +0800

    修改pom文件,将依赖包放在lib下

--- springbootsample/src/main/java/com/simplebank/SpringbootsampleApplication.java
+++ springbootsample/src/main/java/com/simplebank/SpringbootsampleApplication.java
@@ -3,12 +3,16 @@ package com.simplebank;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootsampleApplication {
 
     public static void main(String[] args) {
+        ApplicationContext configurableApplicationContext =
         SpringApplication.run(SpringbootsampleApplication.class, args);
+//        configurableApplicationContext.getEnvironment().getProperty("spring")
     }
 
 }
